b3f84f592fec8e8b5e25788415db0bf8a34512ce
James Leakos <jamesleakos@gmail.com>
2022-12-24 16:04:17 -0600

    added some stuff

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './styles/Navbar.css';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
 
   return (
     <div className='navbar'>
-      <h1>Navbar</h1>
+      <div className='navbar-item' style={{gridColumn: 2, borderWidth: '0 1px'}}>
+        <Link className='links-item reacting-link' to="/posts">About</Link>
+      </div>
+      <div className='navbar-item' style={{gridColumn: 3, borderWidth: '0 1px 0 0'}}>
+        <Link className='links-item reacting-link' to="/posts">Resume</Link>
+      </div>
+      <div className='navbar-title' style={{gridColumn: 4}}>
+        <h3>JAMES LEAKOS</h3>
+      </div>
+      <div className='navbar-item' style={{gridColumn: 5, borderWidth: '0 1px'}}>
+        <Link className='links-item reacting-link' to="/posts">Journal</Link>
+      </div>
+      <div className='navbar-item' style={{gridColumn: 6, borderWidth: '0 1px 0 0'}}>
+        <Link className='links-item reacting-link' to="/posts">Contact</Link>
+      </div>
+
     </div>
   )
 }
--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -6,8 +6,14 @@ import PostList from '../components//PostList.jsx';
 import Info from '../components/Info.jsx';
 
 function Posts() {
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  
   return (
     <div className='posts'>
+      <Navbar />
       <PostList />
       <Info />
       <Footer />
