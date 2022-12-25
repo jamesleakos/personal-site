9e414e002fb47cd653f069f432e3754d6fffcd83
James Leakos <jamesleakos@gmail.com>
2022-12-25 16:43:10 -0600

    some work on textarea

--- client/src/builder-components/MainTitleComp.jsx
+++ client/src/builder-components/MainTitleComp.jsx
@@ -1,10 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './styles/MainTitleComp.css';
+
+
+function TextAreaExpander() {
+  const textareaRef = useRef(null);
+
+  const handleExpand = () => {
+    // Get the number of lines in the textarea
+    const lines = textareaRef.current.value.split('\n').length;
+
+    // Set the rows attribute to the number of lines
+    textareaRef.current.rows = lines;
+  };
+
+  return (
+    <textarea ref={textareaRef} onChange={handleExpand} />
+  );
+}
 
 function MainTitleComp({ component }) {
   return (
     <div className='main-title'>
-      MainTitleComp
+			<TextAreaExpander />
     </div>
   )
 }
