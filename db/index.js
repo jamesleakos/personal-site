66f4f568ca5d41e431fe4af5949ae8cfb81feb37
James Leakos <jamesleakos@gmail.com>
2023-06-25 11:05:39 -0600

    not much

--- db/index.js
+++ db/index.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 
 const mongoURI = `mongodb+srv://${process.env.MONGO_CLOUD_ATLAS_USER}:${process.env.MONGO_CLOUD_ATLAS_PASSWORD}@${process.env.MONGO_CLOUD_ATLAS_URL}/${process.env.MONGO_CLOUD_ATLAS_DB}`;
 
-console.log(process.env.MONGO_CLOUD_ATLAS_DB)
+console.log(process.env.MONGO_CLOUD_ATLAS_DB);
 
+// connect to cloud atlas
 const db = mongoose.connect(mongoURI, { useNewUrlParser: true });
+
+// connect to local db
+// const localURL = process.env.LOCAL_MONGO_URL;
+// const db = mongoose.connect(localURL, { useNewUrlParser: true });
 
 db
   .then(db => console.log(`Connected to: ${mongoURI}`))
